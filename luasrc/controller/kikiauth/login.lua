81d35c93375a75c3be63059ba60a5998587f001c
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2012-08-07 17:29:37 +0700

    Change whitespaces.

--- luasrc/controller/kikiauth/login.lua
+++ luasrc/controller/kikiauth/login.lua
@@ -1,8 +1,8 @@
 module("luci.controller.kikiauth.login", package.seeall)
 
 function index()
-	entry({"kikiauth", "login"}, call("show_login_form"), "Login", 10).dependent=false
-	entry({"kikiauth", "ping"}, call("pingpong"), "", 10).dependent=false
+	entry({"kikiauth", "login"}, call("show_login_form"), "Login", 10).dependent = false
+	entry({"kikiauth", "ping"}, call("pingpong"), "", 10).dependent = false
 end
 
 function show_login_form()

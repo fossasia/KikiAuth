e507db97d7085d270733a01044dbd2cd9b2bb17d
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2012-08-11 11:00:43 +0700

    First implement function to response about user login status.

--- luasrc/controller/kikiauth/authserver.lua
+++ luasrc/controller/kikiauth/authserver.lua
@@ -1,13 +1,13 @@
 module("luci.controller.kikiauth.authserver", package.seeall)
-                                                                                                               
-function index() 
+
+function index()
     entry({"kikiauth", "ping"}, call("action_say_pong"), "Click here", 10).dependent=false
     entry({"kikiauth", "auth"}, call("action_auth_response_to_gw"), "", 20).dependent=false
     entry({"kikiauth", "portal"}, call("action_redirect_to_success_page"), "Success page", 30).dependent=false
     entry({"kikiauth", "login"}, template("kikiauth/login"), "Login page", 40).dependent=false
     entry({"kikiauth", "oauth", "googlecallback"}, template("kikiauth/googlecallback"), "", 50).dependent=false
     entry({"kikiauth", "oauth", "facebookcallback"}, template("kikiauth/facebookcallback"), "", 60).dependent=false
-end                                                                                                            
+end
 
 function action_say_pong()
     luci.http.prepare_content("text/plain")
@@ -16,8 +16,20 @@ end
 
 function action_redirect_to_success_page()
     luci.http.redirect("http://mbm.vn")
-end                                                                   
+end
 
-function action_auth_resonse_to_gw()
-
+function action_auth_response_to_gw()
+	local token = luci.http.formvalue("token")
+	local wget = assert(io.popen("wget --no-check-certificate -qO- \
+	                              https://graph.facebook.com/me?access_token=%s"
+	                              % {token}))
+	if wget then
+		local l = wget:read("*all")
+		wget:close()
+	end
+	if l then
+		luci.http.write("Auth: 1")
+	else
+		luci.http.write("Auth: 6")
+	end
 end

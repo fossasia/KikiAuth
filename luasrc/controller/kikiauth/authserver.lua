18b5d12097d12fd14238a3db8d6a8b4fd9c5ee0f
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2012-08-21 14:50:52 +0700

    merge with Quan's code (from the most current commit: 'Add functions to test if iptables chain exist'

--- luasrc/controller/kikiauth/authserver.lua
+++ luasrc/controller/kikiauth/authserver.lua
@@ -1,5 +1,40 @@
 module("luci.controller.kikiauth.authserver", package.seeall)
 
+-- Name of iptable chain, in which we will open access
+-- to OAuth services (Facebook, Google).
+-- This chain will be in NAT table and FILTER table.
+local chain = "WiFiDog_eth0_OAuthServices"
+
+
+-- === String utilities ====
+
+-- Remove leading and trailing whitespaces from string
+function string:strip()
+	local t = self:gsub("^ +", "")
+	t = t:gsub(" +$", "")
+	return t
+end
+
+-- Check if a string starts with given prefix
+function string.startswith(self, prefix)
+	local ret = false
+	if prefix ~= nil then
+		ret = (self:sub(1, string.len(prefix)) == prefix)
+	end
+	return ret
+end
+
+-- Check if a string ends with given suffix
+function string.endswith(self, suffix)
+	local ret = false
+	if suffix ~= nil then
+		local offset = self:len() - suffix:len()
+		ret = (self:sub(offset + 1) == suffix)
+	end
+	return ret
+end
+
+
 function index()
     entry({"kikiauth", "ping"}, call("action_say_pong"), "Click here", 10).dependent=false
     entry({"kikiauth", "auth"}, call("action_auth_response_to_gw"), "", 20).dependent=false
@@ -18,16 +53,16 @@ function action_say_pong()
     -- It first get out the day and time in the settings, and then,
     -- if it's time to check it will check.
     local uci = require "luci.model.uci".cursor()
-    local day, time, search_pattern                                                                          
-    day = uci:get("kikiauth", "facebook", "day")                                
-    time= uci:get("kikiauth", "facebook", "time")                               
-    -- search_pattern is for 'time' checking. In this situation, 
-    -- we want to check if the current time and 
+    local day, time, search_pattern
+    day = uci:get("kikiauth", "facebook", "day")
+    time= uci:get("kikiauth", "facebook", "time")
+    -- search_pattern is for 'time' checking. In this situation,
+    -- we want to check if the current time and
     -- the one in the setting is different to each other within the range of 3 minutes.
     search_pattern = time..":0[012]"
     local sys = require "luci.sys"
     --check if the current day and time match the ones in the settings.
-    if string.find(os.date(),day) ~= nil or day == "Every" 
+    if string.find(os.date(),day) ~= nil or day == "Every"
             and string.find(os.date(), search_pattern) ~= nil then
         check_fb_ip()
     end
@@ -55,12 +90,12 @@ function action_auth_response_to_gw()
 end
 function check_fb_ip2()
     local httpc = require "luci.httpclient"
-    local uci = require "luci.model.uci".cursor()                               
+    local uci = require "luci.model.uci".cursor()
     local ips = {}
     ips = uci:get_list("kikiauth","facebook","ips")
     for i=1,# ips do
         -- the "if" is used to fix the bug of accessing a nil value of the "ips" table
-        -- (because when one element is removed, 
+        -- (because when one element is removed,
         -- the length of the ips table is correspondingly subtracted by 1).
     	if ips[i] == nil then
     	    break
@@ -75,7 +110,7 @@ function check_fb_ip2()
             i = i - 1
         end
     end
-    for i=1,# ips do 
+    for i=1,# ips do
         print(i, ips[i])
     end
 end
@@ -86,8 +121,8 @@ function check_fb_ip()
     ips = uci:get_list("kikiauth","facebook","ips")
     local sys = require "luci.sys"
     for i=1,# ips do
-        -- the "if" is used to fix the bug of accessing a nil value of the "ips" table       
-        -- (because when one element is removed,                                             
+        -- the "if" is used to fix the bug of accessing a nil value of the "ips" table
+        -- (because when one element is removed,
         -- the length of the ips table is correspondingly subtracted by 1).
       	if ips[i] == nil then
       	    break
@@ -96,12 +131,72 @@ function check_fb_ip()
 	if string.find(output,"64") == nil then
 	    table.remove(ips, i)
 	    -- we have to subtract "i" by 1 to keep track of the correct index of the 'ips' t
-            -- that we want to loop in the next route because after removing an element,     
-            -- the next element will fill the removed position.  
+            -- that we want to loop in the next route because after removing an element,
+            -- the next element will fill the removed position.
 	    i = i - 1
 	end
     end
     uci:set_list("kikiauth","facebook","ips",ips)
     uci:save("kikiauth")
     uci:commit("kikiauth")
+end
+
+-- Get IP list for an OAuth service.
+-- @param service "facebook" or "google"
+function get_oauth_ip_list(service)
+	local x = luci.model.uci.cursor()
+	local lip = x:get_list('kikiauth', 'facebook', 'ips')
+	local allip = {}
+	-- Convert from hostname (www-slb-10-01-prn1.facebook.com)
+	-- to IP.
+	for n, ip in ipairs(lip) do
+		-- Check if ip is a hostname
+		if not ip:match('^%d+.%d+.%d+.%d+$') then
+			allip = extendtable(allip, hostname_to_ips(ip))
+		else
+			table.insert(allip, ip)
+		end
+	end
+	return allip
+end
+
+function hostname_to_ips(host)
+	local l = {}
+	local rs = nixio.getaddrinfo(host, 'inet', 'https')
+	if not rs then
+		return l;
+	end
+	for i, r in pairs(rs) do
+		if r.socktype == 'stream' then table.insert(l, r.address) end
+	end
+	return l
+end
+
+function extendtable(t1, t2)
+	for i, v in pairs(t2) do
+		table.insert(t1, v)
+	end
+	return t1
+end
+
+function iptables_open_access()
+
+end
+
+function iptables_kikiauth_chain_exist()
+	return (iptables_kikiauth_chain_exist_in_table('nat')
+	        and iptables_kikiauth_chain_exist_in_table('filter'))
+end
+
+function iptables_kikiauth_chain_exist_in_table(tname)
+	local count = 0
+	for line in luci.util.execi("iptables-save -t %s | grep %s" % {tname, chain}) do
+		line = line:strip()
+		if count == 0 and line:startswith(":%s" % {chain}) then
+			count = count + 1
+		elseif count == 1 and line:endswith("-j %s" % {chain}) then
+			count = count + 1
+		end
+	end      -- If check OK, count == 2 now
+	return (count > 1)
 end

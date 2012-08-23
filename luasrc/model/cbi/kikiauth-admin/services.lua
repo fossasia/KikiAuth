355fe343589b6e2d3c07d22fecf9c847a2ed9237
Nguyễn Hồng Quân <ng.hong.quan@gmail.com>
2012-08-23 18:19:24 +0700

    Allow to update iptables when list of Facebook IPs change.

--- luasrc/model/cbi/kikiauth-admin/services.lua
+++ luasrc/model/cbi/kikiauth-admin/services.lua
@@ -12,11 +12,33 @@ You may obtain a copy of the License at
 
 $Id$
 ]]--
+authserver = require "luci.controller.kikiauth.authserver"
+
 m = Map("kikiauth", "KikiAuth", translate("KikiAuth creates a captive portal to work with WifiDog. KikiAuth support logging in with popular OAuth services account.")) -- We want to edit the uci config file /etc/config/kikiauth
 
 s = m:section(NamedSection, "facebook", "oauth_services", "Facebook",
               translate("You can register your own Facebook app and use its parameters here."))
-s:option(Flag, "enabled", translate("Enabled?"))
+e = s:option(Flag, "enabled", translate("Enabled?"))
+
+function e.write(self, section, value)
+	if value == '1' then
+		if not authserver.iptables_kikiauth_chain_exist() then
+			authserver.iptables_kikiauth_create_chain()
+		end
+	end
+	Flag.write(self, section, value)
+end
+
+function e.remove(self, section)
+	local dd = os.date('%H %M %S')
+	local l = "%s remove %s" % {dd, section}
+	luci.sys.call("echo '%s' >> /tmp/log_kikiauth.txt" % {l})
+	authserver.iptables_kikiauth_delete_chain()
+	Flag.remove(self, section)
+end
+
+
+---***---
 p = s:option(Value, "app_id", "App ID/ Client ID")
 p:depends('enabled', '1')
 p.default = '420756987974770'
@@ -24,6 +46,8 @@ p = s:option(Value, "redirect_uri", "Redirect URI",
              translate("This URI has to be match the one you registered for your Facebook app."))
 p:depends('enabled', '1')
 p.default = 'http://openwrt.lan/cgi-bin/luci/kikiauth/oauth/facebookcallback'
+
+---***---
 p = s:option(DynamicList, "ips", "Facebook IPs",translate("List of Facebook IPs used for the gateway to open the traffic correctly while using Facebook OAuth."))
 p:depends('enabled', '1')
 p = s:option(Flag, "check_enabled", translate("Periodically check the Facebook IPs list?"))
@@ -65,6 +89,31 @@ p:value("22", translate("22"))
 p:value("23", translate("23"))
 p:value("24", translate("24"))
 
+function p.write(self, section, value)
+	if authserver.iptables_kikiauth_chain_exist() then
+		local added = authserver.iptables_kikiauth_get_ip_list()
+		for _, address in ipairs(value) do
+			authserver.iptables_kikiauth_add_iprule(address, added)
+		end
+	end
+	DynamicList.write(self, section, value)
+end
+
+function p.remove(self, section)
+	-- Get only old IPs of this section. Don't touch other's.
+	old_own_ips = self.map:get(section, self.option)
+	--[[
+	local l = ""
+	for k, v in pairs(old_own_ips) do
+		l = l .. k .. tostring(v)
+	end
+	luci.sys.call("echo 'Old IP %s' >> /tmp/log_kikiauth.txt" % {l})
+	--]]
+	old_own_ips = authserver.to_ip_list(old_own_ips)
+	DynamicList.remove(self, section)
+end
+
+---***---
 s = m:section(NamedSection, "google", "oauth_services", "Google",
               translate("You can register your own Google app and use its parameters here."))
 s:option(Flag, "enabled", translate("Enabled?"))

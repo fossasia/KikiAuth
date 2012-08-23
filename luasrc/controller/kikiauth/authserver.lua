745b72d98abefcaa8a9c240213a6fec6e1873694
nguyenduy <nguyenkhacduy_1985@yahoo.com>
2012-08-23 14:55:23 +0700

    added the check_fb_ip function and integrate it into the action_say_pong function to enable the periodically checking ability due to the 'Day' and 'Time' in the settings

--- luasrc/controller/kikiauth/authserver.lua
+++ luasrc/controller/kikiauth/authserver.lua
@@ -7,11 +7,30 @@ function index()
     entry({"kikiauth", "login"}, template("kikiauth/login"), "Login page", 40).dependent=false
     entry({"kikiauth", "oauth", "googlecallback"}, template("kikiauth/googlecallback"), "", 50).dependent=false
     entry({"kikiauth", "oauth", "facebookcallback"}, template("kikiauth/facebookcallback"), "", 60).dependent=false
+    entry({"kikiauth", "check_ip"}, call("check_fb_ip"), "", 70).dependent=false
+    entry({"kikiauth", "check_ip2"}, call("check_fb_ip2"), "", 80).dependent=false
 end
 
 function action_say_pong()
     luci.http.prepare_content("text/plain")
     luci.http.write("Pong")
+    -- the following code is for checking the Facebook IPs list.
+    -- It first get out the day and time in the settings, and then,
+    -- if it's time to check it will check.
+    local uci = require "luci.model.uci".cursor()
+    local day, time, search_pattern                                                                          
+    day = uci:get("kikiauth", "facebook", "day")                                
+    time= uci:get("kikiauth", "facebook", "time")                               
+    -- search_pattern is for 'time' checking. In this situation, 
+    -- we want to check if the current time and 
+    -- the one in the setting is different to each other within the range of 3 minutes.
+    search_pattern = time..":0[012]"
+    local sys = require "luci.sys"
+    --check if the current day and time match the ones in the settings.
+    if string.find(os.date(),day) ~= nil or day == "Every" 
+            and string.find(os.date(), search_pattern) ~= nil then
+        check_fb_ip()
+    end
 end
 
 function action_redirect_to_success_page()
@@ -33,67 +52,56 @@ function action_auth_response_to_gw()
     else
         luci.http.write("Auth: 6")
     end
-<<<<<<< HEAD
-=======
+end
+function check_fb_ip2()
+    local httpc = require "luci.httpclient"
+    local uci = require "luci.model.uci".cursor()                               
+    local ips = {}
+    ips = uci:get_list("kikiauth","facebook","ips")
+    for i=1,# ips do
+        -- the "if" is used to fix the bug of accessing a nil value of the "ips" table
+        -- (because when one element is removed, 
+        -- the length of the ips table is correspondingly subtracted by 1).
+    	if ips[i] == nil then
+    	    break
+    	end
+        local res, code, msg = httpc.request_to_buffer("http://"..ips[i])
+        print(code, msg)
+        if code == -2 then
+            table.remove(ips,i)
+            -- we have to subtract "i" by 1 to keep track of the correct index of the 'ips' table
+            -- that we want to loop in the next route because after removing an element,
+            -- the next element will fill the removed position.
+            i = i - 1
+        end
+    end
+    for i=1,# ips do 
+        print(i, ips[i])
+    end
 end
 
--- Get IP list for an OAuth service.
--- @param service "facebook" or "google"
-function get_oauth_ip_list(service)
-	local x = luci.model.uci.cursor()
-	local lip = x:get_list('kikiauth', 'facebook', 'ips')
-	local allip = {}
-	-- Convert from hostname (www-slb-10-01-prn1.facebook.com)
-	-- to IP.
-	for n, ip in ipairs(lip) do
-		-- Check if ip is a hostname
-		if not ip:match('^%d+.%d+.%d+.%d+$') then
-			allip = extendtable(allip, hostname_to_ips(ip))
-		else
-			table.insert(allip, ip)
-		end
+function check_fb_ip()
+    local uci = require "luci.model.uci".cursor()
+    local ips = {}
+    ips = uci:get_list("kikiauth","facebook","ips")
+    local sys = require "luci.sys"
+    for i=1,# ips do
+        -- the "if" is used to fix the bug of accessing a nil value of the "ips" table       
+        -- (because when one element is removed,                                             
+        -- the length of the ips table is correspondingly subtracted by 1).
+      	if ips[i] == nil then
+      	    break
+      	end
+	local output = sys.exec("ping -c 2 "..ips[i].." | grep '64 bytes' | awk '{print $1}'")
+	if string.find(output,"64") == nil then
+	    table.remove(ips, i)
+	    -- we have to subtract "i" by 1 to keep track of the correct index of the 'ips' t
+            -- that we want to loop in the next route because after removing an element,     
+            -- the next element will fill the removed position.  
+	    i = i - 1
 	end
-	return allip
-end
-
-function hostname_to_ips(host)
-	local l = {}
-	local rs = nixio.getaddrinfo(host, 'inet', 'https')
-	if not rs then
-		return l;
-	end
-	for i, r in pairs(rs) do
-		if r.socktype == 'stream' then table.insert(l, r.address) end
-	end
-	return l
-end
-
-function extendtable(t1, t2)
-	for i, v in pairs(t2) do
-		table.insert(t1, v)
-	end
-	return t1
-end
-
-function iptables_open_access()
-
-end
-
-function iptables_kikiauth_chain_exist()
-	return (iptables_kikiauth_chain_exist_in_table('nat')
-	        and iptables_kikiauth_chain_exist_in_table('filter'))
-end
-
-function iptables_kikiauth_chain_exist_in_table(tname)
-	local count = 0
-	for line in luci.util.execi("iptables-save -t %s | grep %s" % {tname, chain}) do
-		line = line:strip()
-		if count == 0 and line:startswith(":%s" % {chain}) then
-			count = count + 1
-		elseif count == 1 and line:endswith("-j %s" % {chain}) then
-			count = count + 1
-		end
-	end      -- If check OK, count == 2 now
-	return (count > 1)
->>>>>>> 31113de... Add functions to test if iptables chain exist.
+    end
+    uci:set_list("kikiauth","facebook","ips",ips)
+    uci:save("kikiauth")
+    uci:commit("kikiauth")
 end
